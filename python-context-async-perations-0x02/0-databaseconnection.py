6b6ba7eb28f341c9c30267a7111d13055e5f82ee
Limah-T <limahtechnology@yahoo.com>
2025-07-11 18:00:20 +0100

    custom class based context manager for Database connection

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+db_file_path = "C:/Users/User/OneDrive/Desktop/alx-backend-python/python-decorators-0x01/user.db"
+
+class DatabaseConnection:
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(db_file_path)       
+        return self.conn
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.conn.close()
+        print(exc_type, exc_value, exc_tb, sep="\n")
+
+with DatabaseConnection() as connection:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user")
+    print(cursor.fetchall())
