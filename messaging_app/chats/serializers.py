886c22059027f29ecded6ef1516f9175e2b2404e
Limah-T <limahtechnology@yahoo.com>
2025-07-21 14:10:02 +0100

    Create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,21 +1,41 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.Serializer):
+    user_id = serializers.UUIDField(format="hex_verbose", read_only=True)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+    email = serializers.EmailField()
+    phone_number = serializers.CharField(max_length=16)
+    created_at = serializers.DateTimeField()
 
-    class Meta:
-        model = User
-        fields = ["user_id", "first_name", "last_name", "email", "phone_number", "created_at"]
+    def create(self, validated_data):
+        firstname = validated_data.get("first_name")
+        last_name = validated_data.get("last_name")
+        email = validated_data.get("email")
+        if not all([firstname, last_name, email]):
+            raise serializers.ValidationError("This field may not be blank")
+        return super().create(validated_data)
+
 
 class MessageSerializer(serializers.ModelSerializer):
+    message_id = serializers.UUIDField(format="hex_verbose", read_only=True)
+    sender_id = serializers.UUIDField(format="hex_verbose", read_only=True)
+    message_body = serializers.CharField(max_length=255)
+    sent_at = serializers.DateTimeField()
 
-    class Meta:
-        model = Message
-        fields = ["message_id", "sender_id.first_name", "message_body", "sent_at"]
+    def create(self, validated_data):
+        message_body = validated_data.get("message_body")
+        if not message_body:
+            raise serializers.ValidationError("This field may not be blank")
+        return super().create(validated_data)
 
 class ConversationSerializer(serializers.ModelSerializer):
-    messages = MessageSerializer()
+    conversation_id = serializers.UUIDField(format="hex_verbose", read_only=True)
+    participant_id = serializers.UUIDField(format="hex_verbose", read_only=True)
+    created_at = serializers.DateTimeField()
+    messages = MessageSerializer(many=True)
 
-    class Meta:
-        model = Conversation
-        fields = ["conversation_id", "participant_id", "created_at"]
+
+
+    
