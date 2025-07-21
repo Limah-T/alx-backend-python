d3aa3825d54140daee8302a6b14d22d44e8387e1
Limah-T <limahtechnology@yahoo.com>
2025-07-21 15:20:52 +0100

    Correct path names to meet checker's expectations.

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,7 @@
 # from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register(r'conversation', ConversationViewSet.as_view(), 'conversations')
-router.register(r'messages', MessageViewSet.as_view(), 'messages')
+router = routers.DefaultRouter()
+router.register(r'api/conversation', ConversationViewSet.as_view(), 'conversations')
+router.register(r'api/messages', MessageViewSet.as_view(), 'messages')
